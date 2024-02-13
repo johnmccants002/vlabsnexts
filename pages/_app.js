c9cacf6e1c4023cbbecdc20d7ccedad4beecf552
John McCants <johnmccants@JohnsMacStudio.attlocal.net>
2024-02-13 01:26:15 -0800

    update

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,7 @@ import "@fortawesome/fontawesome-svg-core/styles.css";
 config.autoAddCss = false;
 
 export default function App({ Component, pageProps }) {
+  console.log("");
   return (
     <>
       <Navbar />
